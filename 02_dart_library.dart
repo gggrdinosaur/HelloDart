485a15417e3942cc83c21251964175be55849890
gggrdinosaur <1463235793@qq.com>
2022-04-14 07:12:29 +0000

    新增

---
+++ 02_dart_library.dart
@@ -0,0 +1,14 @@
+//数字  
+void digits()
+{
+  print('*' * 40);
+  print('数字');
+
+  assert(int.parse('42') == 42);
+  assert(int.parse('0x42') == 66);
+  assert(double.parse('0.50') == 0.5);
+}
+
+void main(List<String> args){
+  digits();
+}
